7f5339167ec0ee72375980e621ee5c6af600f293
Neto test <pashtet201807@gmail.com>
2026-04-06 17:11:09 +0300

    correction2

--- src/test/java/ru/netology/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -16,6 +16,20 @@ class FreelancerServiceTest {
         FreelancerService service = new FreelancerService();
         int actual = service.calculate(income, expenses, threshold);
 
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testCalculate2() {
+
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        FreelancerService service = new FreelancerService();
+        int actual = service.calculate(income, expenses, threshold);
+
         Assertions .assertEquals(expected, actual);
     }
 }
